2d26080da24b9ea1be601b583bc303ba76267dfa
周熙岩 <zxycat2@zhouxiyandemacbook-pro.local>
2019-01-14 21:56:48 +0000

    Initial Commit

---
+++ Party Dice/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Party Dice
+//
+//  Created by 周熙岩 on 2019/1/14.
+//  Copyright © 2019 DoDo. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Party Dice/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Party Dice
+//
+//  Created by 周熙岩 on 2019/1/14.
+//  Copyright © 2019 DoDo. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
