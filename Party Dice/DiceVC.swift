77d5ec8f13f1089f8057469a7691b7e34ee01480
周熙岩 <zxycat2@zhouxiyandemacbook-pro.local>
2019-01-15 17:24:19 +0000

    Finished but it froze when throwing 4 or more dice

--- Party Dice/DiceVC.swift
+++ Party Dice/DiceVC.swift
@@ -11,67 +11,85 @@ import UIKit
 class DiceVC: UIViewController {
     
     var diceLength:CGFloat = 0
-
+    var numberOfDice = 1{
+        didSet{
+            self.diceNumberLabel.text = String(self.numberOfDice)
+        }
+    }
+    var allDiceViews:[DiceView] = []
+    let diceMoveTime = 0.5
+    let numberOfRoundsTheDiceRolls:CGFloat = 3
     override func viewDidLoad() {
         super.viewDidLoad()
         
-
-
-        // Do any additional setup after loading the view.
     }
-    var firstTime = true
     override func viewDidLayoutSubviews() {
-        print("did layout subviews")
         //初始化骰子边长
         self.diceLength = (self.view.bounds.width)/6
-        //test
-        if firstTime{
-            
-            let pos = getRandomLandingPosition(numberOfPositions: 3)
-            for stuff in pos{
-                let v = DiceView(frame:stuff)
-                v.backgroundImage = UIImage(named: "one")
-                self.deckView.addSubview(v)
-            }
-            self.firstTime = false
-        }
     }
     
     @IBAction func rollButton(_ sender: UIButton) {
-        for view in self.deckView.subviews{
-            view.removeFromSuperview()
+//        for view in self.deckView.subviews{
+//            view.removeFromSuperview()
+//        }
+//        let pos = getRandomLandingPosition(numberOfPositions: 3)
+//        for stuff in pos{
+//            let v = DiceView(frame:stuff)
+//            v.backgroundImage = UIImage(named: "one")
+//            self.deckView.addSubview(v)
+//        }
+        //先搞掉已经存在的骰子
+        for diceView in self.deckView.subviews{
+            diceView.removeFromSuperview()
         }
-        let pos = getRandomLandingPosition(numberOfPositions: 3)
-        for stuff in pos{
-            let v = DiceView(frame:stuff)
-            v.backgroundImage = UIImage(named: "one")
-            self.deckView.addSubview(v)
+        self.allDiceViews = []
+        //生成，放到左上角
+        for _ in 1...self.numberOfDice{
+            let diceView = DiceView(frame:CGRect(x: 0, y: 0, width: self.diceLength, height: self.diceLength))
+            self.allDiceViews.append(diceView)
+            self.deckView.addSubview(diceView)
+            let randomNumber = arc4random_uniform(6) + 1
+            var diceImage = UIImage(named: "pic")
+            switch randomNumber{
+            case 1:
+                diceImage = UIImage(named: "one")
+            case 2:
+                diceImage = UIImage(named: "two")
+            case 3:
+                diceImage = UIImage(named: "three")
+            case 4:
+                diceImage = UIImage(named: "four")
+            case 5:
+                diceImage = UIImage(named: "five")
+            case 6:
+                diceImage = UIImage(named: "six")
+            default:
+                diceImage = UIImage(named: "pic")
+            }
+            diceView.backgroundImage = diceImage
+            
         }
+        //动画丢
+        let newFrames = getRandomLandingPosition(numberOfPositions: self.numberOfDice)
+        for index in 0...self.numberOfDice-1{
+            UIView.transition(with: self.allDiceViews[index], duration: self.diceMoveTime, options: [], animations:{ self.allDiceViews[index].frame = newFrames[index]
+                self.allDiceViews[index].transform = CGAffineTransform(rotationAngle: self.numberOfRoundsTheDiceRolls*CGFloat.pi)
+            }, completion: nil)
+        }
+        
     }
     
     @IBAction func addDiceButton(_ sender: Any) {
+        self.numberOfDice+=1
     }
     @IBAction func minusDiceButton(_ sender: Any) {
+        self.numberOfDice-=1
     }
-    @IBOutlet weak var diceNumberLabel: UIStackView!
+    
+    @IBOutlet weak var diceNumberLabel: UILabel!
     
     @IBOutlet weak var deckView: DiceDeckView!
     
-    //返回包含9个CGRECT的列表用来掷骰子
-//    func getAllPossibleLandingLocation() -> [CGRect] {
-//        let minusNumber:CGFloat = 20
-//
-//        let boundWidth = Int(self.deckView.bounds.width)
-//        let boundHeight = Int(self.deckView.bounds.height)
-//
-//        var possibleLandingLocations:[CGRect] = []
-//        for outterIndex in 1...3{
-//            for innerIndex in 1...3{
-//                possibleLandingLocations.append(CGRect(x: CGFloat(outterIndex*(boundWidth/3)) - self.diceLength - minusNumber, y: CGFloat(innerIndex*(boundHeight/3)) - self.diceLength - minusNumber, width: self.diceLength, height: self.diceLength))
-//            }
-//        }
-//        return possibleLandingLocations
-//    }
     
     //返回n个随机位置
     func getRandomLandingPosition(numberOfPositions:Int)->[CGRect]{
@@ -102,7 +120,6 @@ class DiceVC: UIViewController {
                 continue
             }
         }
-        //随机角度旋转
         return returnCGRects
     }
     
